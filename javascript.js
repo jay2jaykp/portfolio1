776a2e69a6c79f923f6bdb62506a5f2c32552518
Jay Patel <jay2jaykp@gmail.com>
2018-02-25 17:54:49 -0500

    big update

--- javascript.js
+++ javascript.js
@@ -1,12 +1,41 @@
-function initMap() {
-    var center1 = {lat: 43.664764, lng:  -79.367940};
-    var map1 = new google.maps.Map(document.getElementById('myMap'), {
-        zoom: 18,
-        center: center1
-    }); 
-    var marker2 = new google.maps.Marker({
-        position: center2,
-        map: map2
-    });
+//navigation become white when scrolling....
+$(window).scroll(function() {
+    if ($(window).width() < 992){
+        if ($(window).scrollTop() > 50) {
+            $('nav').addClass('w3-white w3-card-4');
+            $('#navbarCollapse').removeClass('w3-white w3-card-4');
+        } else {
+            $('nav').removeClass('w3-white w3-card-4');
+            $('#navbarCollapse').addClass('w3-white w3-card-4');
+
+        }
+    } else {
+        if ($(window).scrollTop() > 50) {
+            $('nav').addClass('w3-white w3-card-4');
+            $('#navbarCollapse').removeClass('w3-white w3-card-4');
+        } else {
+            $('nav').removeClass('w3-white w3-card-4');
+        }
+    }
+});
+
+if($(window).width() < 991) {
+    $('#navbarCollapse').addClass('w3-white w3-card-4');
+} else {
+    $('#navbarCollapse').removeClass('w3-white w3-card-4');
+
 }
 
+
+
+
+//
+var $root = $('html, body');
+
+$('a[href^="#"]').click(function () {
+    $root.animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 500);
+
+    return false;
+});
